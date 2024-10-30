6bd1312c81c6d0a33fc93e9ced6cef15ee89b1a4
amsuccy <amiedesuccess123@gmail.com>
2024-10-30 15:13:55 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 
 import Header from './components/Header'
+
 import { BrowserRouter, Routes, Route   } from 'react-router-dom';
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
+
 
 function App() {
  
@@ -26,7 +28,7 @@ function App() {
     
    
     </Routes>
-    <Footer/>
+    {/* <Footer /> */}
    </BrowserRouter>
   )
 }
